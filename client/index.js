721a51d6a1a5dd146d5ebec999d136284ff65f62
Sindhu <ksindhu571@gmail.com>
2023-12-25 21:49:38 +0530

    Working model

--- client/index.js
+++ client/index.js
@@ -1,15 +1,1 @@
-import { io } from "socket.io-client";
-
-const socket = io("wss://localhost:8080");
-
-socket.on("message", (incomingMessage) => {
-    let element = document.createElement('li');
-    element.innerHTML = incomingMessage;
-    document.querySelector('ul').appendChild(element);
-});
-
-
-document.querySelector('button').on("click", () =>{
-    const outgoingMessage = document.querySelector('input').value;
-    socket.emit('message',outgoingMessage);
-})
+const socket = io();
--- server/index.js
+++ server/index.js
@@ -1,16 +1,29 @@
-import {createServer} from "http";
 import { Server } from "socket.io";
+import { createServer } from "http";
+import express from 'express';
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
-const httpServer = createServer();
+
+const app = express();
+const httpServer = createServer(app); 
 const io = new Server(httpServer,{});
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const clientPath = path.resolve(__dirname, "../client")
 
-io.on("connection",(socket) => {
-    console.log("User connected.");
-    socket.on("message",(incomingMessage)=>{
-        console.log(incomingMessage);
-    })
+
+app.use(express.static(clientPath));
+app.get('/',(request, response)=>{
+    response.sendFile(clientPath + "/index.html");
 })
 
-httpServer.listen(8080, () => {
-    console.log("Listening on http://localhost:8080");
+
+io.on("connection", (socket) => {
+    console.log("A user connected. ", socket.id);
+})
+
+
+httpServer.listen(3000, () => {
+    console.log("Server started at http://localhost:3000");
 })
